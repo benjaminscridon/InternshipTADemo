58fe34d59db91656441d7c3aef1b96480e898f94
bScridon <beniamin.scridon@8x8.com>
2016-08-23 15:04:24 +0300

    Add .gitignore file

---
+++ src/test/java/stepdefinitions/AmazonSteps.java
@@ -0,0 +1,16 @@
+package stepdefinitions;
+
+import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by bscridon on 23.08.2016.
+ */
+public class AmazonSteps {
+
+    private WebDriver driver;
+
+
+
+
+}
---
+++ src/test/resources/features/Amazon.feature
@@ -0,0 +1,8 @@
+Feature: Amazon search feature
+  As a <role>,
+    I want to <goal/desire>,
+      so that <benefit>
+
+  Scenario: Amazon.Demo.Search
+    Given the following page is displayed "http://amazon.com"
+
