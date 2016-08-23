aff97b77a48951e2467249b6738934dcb5ccdce1
bScridon <beniamin.scridon@8x8.com>
2016-08-23 16:19:14 +0300

    Final TA

--- src/test/java/stepdefinitions/AmazonSteps.java
+++ src/test/java/stepdefinitions/AmazonSteps.java
@@ -1,9 +1,15 @@
 package stepdefinitions;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -26,14 +32,38 @@ public class AmazonSteps {
 
 
     @After("@chrome")
-    public void stopChrome(){
+    public void stopChrome() {
         //driver.quit();
     }
 
 
     @Given("^the following page is displayed \"([^\"]*)\"$")
-    public void theFollowingIsDisplayed(String baseUrl){
-
+    public void theFollowingIsDisplayed(String baseUrl) {
+        driver.get(baseUrl);
     }
 
+    @When("^\"([^\"]*)\" is entered in search bar$")
+    public void isEnteredInSearchBar(String searchTerm) throws Throwable {
+        WebElement mainSearchBar = driver.findElement(By.id("twotabsearchtextbox"));
+        mainSearchBar.clear();
+        mainSearchBar.sendKeys(searchTerm);
+    }
+
+    @When("^Search button is pressed$")
+    public void searchButtonIsPressed() throws Throwable {
+        WebElement mainSearchButton = driver.findElement(By.xpath("//input[@value='Go']"));
+        mainSearchButton.click();
+    }
+
+    @Then("^the \"([^\"]*)\" result title should be \"([^\"]*)\"$")
+    public void theResultTitleShouldBe(int resultPosition, String expectedResultTitle) throws Throwable {
+        String actualResultTitle = buildSearchResultLocator(resultPosition).getText();
+        Assert.assertEquals(expectedResultTitle,actualResultTitle);
+    }
+
+    public WebElement buildSearchResultLocator(int searchResultPosition) {
+        String searchResultXpath = "//*[@id='result_" + (searchResultPosition - 1) + "']//h2";
+        WebElement searchResult = driver.findElement(By.xpath(searchResultXpath));
+        return searchResult;
+    }
 }
--- src/test/resources/features/Amazon.feature
+++ src/test/resources/features/Amazon.feature
@@ -6,4 +6,12 @@ Feature: Amazon search feature
   @chrome
   Scenario: Amazon.Demo.Search
     Given the following page is displayed "http://amazon.com"
+      When "Software Testing" is entered in search bar
+        When Search button is pressed
+          Then the "1" result title should be "Software Testing (2nd Edition)"
+          Then the "5" result title should be "Software Testing: An ISTQB-BCS Certified Tester Foundation Guide 3rd ed"
+
+
+
+
 
